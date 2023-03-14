7f731bfe3d76a66892395d8f98e719571df224e6
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-14 21:33:11 +0500

    Slicing03

--- Slicing03.py
+++ Slicing03.py
@@ -1,8 +1,11 @@
 def main(s):
-    """The s string variable is given. Return all characters except the one at the beginning and end.
+    """
+    The s string variable is given. Return all characters except the one at the beginning and end.
     Args:
         s(str): parameter
     Returns:
         str: answer
     """
-    return
+    return s[1:-1]
+
+print(main("visualstudio"))
